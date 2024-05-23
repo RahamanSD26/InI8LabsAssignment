ceabf70cae42f9feff5124f531e591cfff4a2eef
Rahaman <rahaman@sidpik.com>
2024-05-23 16:37:10 +0530

    Registration web application version 1 completed

---
+++ src/main/java/com/INI8/crud/assignment/AssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.INI8.crud.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/INI8/crud/assignment/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.INI8.crud.assignment.service;
+
+import java.util.List;
+
+import com.INI8.crud.assignment.model.Employee;
+import org.springframework.data.domain.Page;
+
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+    Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+}
